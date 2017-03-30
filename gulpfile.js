4f865a8215b00a628f0244c9ac8ecdc192e65c6b
冯维 <wei.feng@socialcreidts.cn>
2017-03-30 14:28:01 +0800

    添加末班文件

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ gulp.task('es5Toes6', function() {
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('watch', function() {
+gulp.task('moveTemp', function() {
+  return gulp.src("./src/temp/*.ejs")
+    .pipe(gulp.dest("./dist/temp"));
+});
+
+gulp.task('watch', ['es5Toes6', 'moveTemp'], function() {
+  gulp.watch(['./src/temp/*.ejs'], ['moveTemp']);
   gulp.watch(['./src/*.js'], ['es5Toes6']);
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import express from 'express';
 
+import url from 'url';
+
 const app = express();
+const PORT = 5000;
+
+app.use(express.static('./static'));
+app.set('view engine', 'ejs');
+app.set('views', './dist/temp');
+
 app.use((req, res, next) =>{
-  return res.end('Hello word123!!!');
+  const reqUrlObj = url.parse(req.url, true);
+  const reqPathName = reqUrlObj.pathname;
+  const reqQueryObj = reqUrlObj.query;
+  console.log(reqUrlObj, '------');
+  if (reqPathName == '/' || reqPathName == '/index') {
+    return res.render('index.ejs');
+  } else if(reqPathName=='/login') {
+    return res.render('login.ejs');
+  } else {
+    return res.render('notfond.ejs');
+  }
 });
-app.listen(5000, function() {
-  console.log('app is listening at 5000!');
+
+app.listen(PORT, function() {
+  console.log(`app is listening at ${PORT}!`);
 });
